01ba63f3f162f52653d031941e802288b96a6357
Wu-Ke-CUC <wuke0704@outlook.com>
2026-01-03 22:02:19 +0800

    Test33,34

--- LeetCode/Algorithm/AlgorithmTest.cpp
+++ LeetCode/Algorithm/AlgorithmTest.cpp
@@ -974,25 +974,65 @@ class Solution
 		int search(vector<int>& nums, int target) 
 		{
 			if (nums.empty())return -1;
-			int left = 0, right = nums.size() - 1, mid = right / 2;
-			if (target >= nums[left])
+			if (nums.size() == 1)
 			{
-				while (left <= mid)
-				{
-					if (nums[left] == target)return left;
-					else left++;
-				}
-				return -1;
+				return nums[0] == target ? 0 : -1;
 			}
-			if (target <= nums[right])
+			int left = 0, right = nums.size() - 1;
+			while (left <= right)
 			{
-				while (right > mid)
+				int mid = (left + right) / 2;
+				if (nums[mid] == target)return mid;
+				if (nums[left] <= nums[mid])
 				{
-					if (nums[right] == target)return right;
-					else right--;
+					if (target >= nums[left] && target <= nums[mid])
+					{
+						right = mid - 1;
+					}
+					else
+					{
+						left = mid + 1;
+					}
 				}
-				return -1;
+				else
+				{
+					if (target <= nums[right] && target > nums[mid])
+					{
+						left = mid + 1;
+					}
+					else
+					{
+						right = mid - 1;
+					}
+				}
 			}
+			return -1;
+		}
+		#pragma endregion
+		#pragma region 34.在数组排序中查找元素的第一个和最后一个位置
+		vector<int> searchRange(vector<int>& nums, int target) {
+			if (nums.empty())return { -1,-1 };
+			int left = 0, right = nums.size() - 1;
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+				if (nums[mid] == target)
+				{
+					int first = mid, last = mid;
+					while (first > 0 && nums[first - 1] == target)first--;
+					while (last < nums.size() - 1 && nums[last + 1] == target)last++;
+					return { first,last };
+				}
+				else if (nums[mid] > target)
+				{
+					right = mid - 1;
+				}
+				else if (nums[mid] < target)
+				{
+					left = mid + 1;
+				}
+			}
+			return { -1,-1 };
 		}
 		#pragma endregion
 
